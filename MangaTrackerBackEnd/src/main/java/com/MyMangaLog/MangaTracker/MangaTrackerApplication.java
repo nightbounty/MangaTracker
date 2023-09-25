a1bfb584709577e205898fa4db9914e8b56fa569
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-09-25 19:42:29 -0400

    Initialize backend

---
+++ MangaTrackerBackEnd/src/main/java/com/MyMangaLog/MangaTracker/MangaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.MyMangaLog.MangaTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaTrackerApplication.class, args);
+	}
+
+}
---
+++ MangaTrackerBackEnd/src/test/java/com/MyMangaLog/MangaTracker/MangaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyMangaLog.MangaTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
