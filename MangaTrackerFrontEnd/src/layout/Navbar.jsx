4325d971680d0c02a6c3b77bba4d9f30f7127f01
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-09-26 15:57:49 -0400

    Added Navbar

---
+++ MangaTrackerFrontEnd/src/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Manga Tracker
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <a className="btn btn-outline-light" href="/adduser">
+            Add Manga
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
