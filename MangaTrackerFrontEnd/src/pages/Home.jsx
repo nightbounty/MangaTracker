962d2fba7003dd9e80e8257f7c6f5bba8d6fcaf1
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-09-26 16:32:27 -0400

    Updated HomePage to display SQL database using AXIOSGET

--- MangaTrackerFrontEnd/src/pages/Home.jsx
+++ MangaTrackerFrontEnd/src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 
 export default function Home() {
+    const [mangas, setMangas] = useState([]);
 
+  
+    useEffect(() => {
+      loadMangas();
+    }, []);
+  
+    const loadMangas = async () => {
+      const result = await axios.get("http://localhost:8080/mangas");
+      setMangas(result.data);
+    };
+  
+ 
+  
   return (
     <div className="container">
       <div className="py-4">
@@ -15,13 +28,15 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            
-              <tr>
-                <td>One Piece</td>
-                <td>Reading</td>
-                <td>1069</td>
-        
-              </tr>
+                {mangas.map((manga,index) => (
+                    <tr>
+                        <td>{manga.title}</td>
+                        <td>{manga.status}</td>
+                        <td>{manga.chapter}</td>
+                      </tr>
+                    ))
+                }
+
            
           </tbody>
         </table>
